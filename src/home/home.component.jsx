12b682572999c893dccd69170566cb9dd046b09e
Silvery86 <giang8692@gmail.com>
2023-05-07 15:13:53 +0700

    add navigation bar by route and style navigation bar

---
+++ src/home/home.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CategoryMenu from '../categories-menu/category-menu.component'
+
+export default function Home() {
+  return (
+    <div>
+      <CategoryMenu/>
+    </div>
+  )
+}
---
+++ src/navigation/navigation.component.jsx
@@ -0,0 +1,21 @@
+import { Link, Outlet } from "react-router-dom"
+import { Fragment } from "react"
+import { ReactComponent as Logo } from "../assets/crown.svg"
+import './navigation.styles.scss'
+export default function Navigation() {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+            <Logo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>SHOP</Link>
+          <Link></Link>
+          <Link></Link>
+        </div>
+      </div>
+        <Outlet/>
+    </Fragment>
+  )
+}
